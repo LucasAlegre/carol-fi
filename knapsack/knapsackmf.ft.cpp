3b604eb60e23d1d6ba7e63caf50a62da0c0a2d03
Lucas Hagen <lucas_hagen@hotmail.com>
2018-11-03 14:56:21 -0300

    Created files for fault tolerant algorithms
    Signed-off-by: Lucas Hagen <lucas_hagen@hotmail.com>

---
+++ knapsack/knapsackmf.ft.cpp
@@ -0,0 +1,83 @@
+// A Memory Functions based solution for 0-1 Knapsack problem
+
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int W;
+int n;
+vector<int> values;
+vector<int> weights;
+vector<vector<int>> V;
+
+int knapSack();
+int knapSackAuxRec(int i, int j);
+
+
+int knapSack() {
+
+    V = vector<vector<int>>(n + 1);
+
+    for (int i = 0; i <= n; i++)
+        V[i] = vector<int>(W + 1);
+
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0) {
+                V[i][w] = 0;
+            } else {
+                V[i][w] = -1;
+            }
+        }
+    }
+
+    V[n][W] = knapSackAuxRec(n, W);
+
+    return V[n][W];
+}
+
+int knapSackAuxRec(int i, int j) { // i = n, j = w
+    int value;
+    if (V[i][j] < 0) { // meaning not already calculated
+
+        if (j < weights[i]) {
+            value = knapSackAuxRec(i - 1, j);
+        } else {
+            value = max(knapSackAuxRec(i - 1, j), values[i] + knapSackAuxRec(i - 1, j - weights[i]));
+        }
+
+
+        V[i][j] = value;// put valid value in the table for both cases
+    }
+
+    return V[i][j];
+}
+
+int main() {
+    ifstream file("/tmp/knapsack/instance.txt");
+    ofstream outfile("/tmp/knapsack/outputmf");
+
+    file >> n;
+    file >> W;
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        file >> x >> y;
+        values.push_back(x);
+        weights.push_back(y);
+    }
+
+    int result = knapSack();
+
+    outfile << result;
+    outfile.close();
+
+    std::cout << "Result: " << result << "\n";
+
+    return 0;
+}
---
+++ knapsack/knapsacksa.ft.cpp
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<sstream>
+#include<string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+using namespace std;
+
+vector<int> val;
+vector<int> wt;
+int n, W;
+
+int rand_bit(){
+    return rand() % 2;
+}
+
+float random01(){
+    return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}
+
+int cost(vector<int> &solution){
+    int peso=0, valor=0;
+    for(int i=0; i < n; i++){
+        peso += solution[i] * wt[i];
+        valor += solution[i] * val[i];
+    }
+    if(peso > W)
+        return 99999999;
+    else
+        return -valor;
+}
+
+int simulated_annealing() {
+    vector<int> solution(n);
+    for(int i = 0; i < n; i++)
+        solution[i] = rand_bit();
+    int best = cost(solution);
+    int temp = 2000000;
+
+    //int co=0;
+
+    for(int t = 0; t < 1000000; t++){
+        vector<int> s(solution);
+        int i = rand() % n;
+        s[i] = 1 - s[i];
+        int c = cost(s);
+        int delta = c - best;
+        if(delta <= 0){
+            solution = s;
+            best = c;
+        }
+        else if (random01() <= exp(-delta/(float)(temp))){
+            //cout << exp(-delta/(float)(temp)) << endl;
+            //co++;
+
+            solution = s;
+            best = min(c, best);
+        }
+        temp *= 0.99999;
+    }
+    //cout << co << endl;
+
+    return -best;
+}
+
+int main()
+{
+    ifstream file("/tmp/knapsack/instance.txt");
+    ofstream outfile("/tmp/knapsack/outputsa");
+
+    srand (42);
+    file >> n; file >> W;
+
+    for(int i = 0; i < n; i++){
+        int x, y; file >> x >> y;
+        val.push_back(x);
+        wt.push_back(y);
+    }
+
+    outfile << simulated_annealing();
+    outfile.close();
+
+    return 0;
+}
