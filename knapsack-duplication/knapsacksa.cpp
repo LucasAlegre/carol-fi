530c648822210c574683f5220118e8e71824c7a8
Lucas Alegre <lucasnale@gmail.com>
2018-11-30 12:31:43 -0200

    Duplication

--- knapsack-duplication/knapsacksa.cpp
+++ knapsack-duplication/knapsacksa.cpp
@@ -4,109 +4,97 @@
 #include<vector>
 #include<sstream>
 #include<string>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h> 
+#include <stdlib.h>     
+#include <time.h>     
 #include <math.h>
 using namespace std;
 
-int W1;
 vector<int> val;
-int W2;
 vector<int> wt;
-int n;
-int W3;
-
-int getW() {
-	if(W1 == W2 && W2 == W3) {
-		return W1;
-	} else if(W1 == W2) {
-		W3 = W1;
-	} else if(W2 == W3) {
-		W1 = W2;
-	} else if(W1 == W3) {
-		W2 = W1;
-	}
-	return W1;
-}
-
-void setW(int newW) {
-	W1 = W2 = W3 = newW;
-}
+int n, W;
 
 int rand_bit(){
     return rand() % 2;
 }
 
 float random01(){
-    return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+   return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 }
 
 int cost(vector<int> &solution){
     int peso=0, valor=0;
     for(int i=0; i < n; i++){
-        peso += solution[i] * wt[i];
-        valor += solution[i] * val[i];
+       peso += solution[i] * wt[i];
+       valor += solution[i] * val[i];
     }
-    if(peso > getW())
-        return 99999999;
+    if(peso > W)
+       return 99999999;
     else
-        return -valor;
+       return -valor;
 }
 
 int simulated_annealing() {
+    srand(42);
+
     vector<int> solution(n);
     for(int i = 0; i < n; i++)
-        solution[i] = rand_bit();
+       solution[i] = rand_bit();
     int best = cost(solution);
     int temp = 2000000;
 
     //int co=0;
 
     for(int t = 0; t < 1000000; t++){
-        vector<int> s(solution);
-        int i = rand() % n;
-        s[i] = 1 - s[i];
-        int c = cost(s);
-        int delta = c - best;
-        if(delta <= 0){
-            solution = s;
-            best = c;
-        }
-        else if (random01() <= exp(-delta/(float)(temp))){
-            //cout << exp(-delta/(float)(temp)) << endl;
-            //co++;
+         vector<int> s(solution);
+         int i = rand() % n;
+         s[i] = 1 - s[i];
+         int c = cost(s);
+         int delta = c - best;
+         if(delta <= 0){
+             solution = s;
+             best = c;
+         }
+         else if (random01() <= exp(-delta/(float)(temp))){
 
-            solution = s;
-            best = min(c, best);
-        }
-        temp *= 0.99999;
+             solution = s;
+             best = min(c, best);
+         }
+         temp *= 0.99999;
     }
-    //cout << co << endl;
-
+    
     return -best;
 }
 
-int main()
-{
+void read_input(){
     ifstream file("/tmp/knapsack/instance.txt");
-    ofstream outfile("/tmp/knapsack/outputsa");
-	
-	int tempW;
-
-    srand (42);
-    file >> n; file >> tempW;
-	
-	setW(tempW);
-
+    val.clear(); wt.clear();
+    file >> n; file >> W;
     for(int i = 0; i < n; i++){
         int x, y; file >> x >> y;
         val.push_back(x);
         wt.push_back(y);
     }
+    file.close();
+}
+ 
+int main()
+{
+    ofstream outfile("/tmp/knapsack/outputsa");
+    read_input();
+    int result1 = simulated_annealing();
+    read_input();
+    int result2 = simulated_annealing();
+    
+    if(result1 != result2) {
+	ofstream detection_log("/tmp/knapsack/sa-detection.log");
+        detection_log << result1 << " " << result2;
+    	detection_log.close();
+    }
 
-    outfile << simulated_annealing();
+    outfile << result1;
     outfile.close();
+    cout << "Result: " << result1 << "\n";
 
     return 0;
 }
