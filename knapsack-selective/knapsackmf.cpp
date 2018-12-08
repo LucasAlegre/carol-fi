16b5c9ef5c7a9c37a6ea29a243091dd86f3b882f
Lucas Hagen <lucas_hagen@hotmail.com>
2018-12-08 00:06:20 -0200

    Added Selective Hardening for MF
    Signed-off-by: Lucas Hagen <lucas_hagen@hotmail.com>

--- knapsack-selective/knapsackmf.cpp
+++ knapsack-selective/knapsackmf.cpp
@@ -9,88 +9,118 @@
 
 using namespace std;
 
-int knapSack(int W, int n, vector<int>& values, vector<int>& weights, vector<vector<int>>& V);
-int knapSackAuxRec(int i, int j, vector<int>& values, vector<int>& weights, vector<vector<int>>& V);
+int W;
+int W2;
+int n;
+int n2;
+vector<int> values;
+vector<int> values2;
+vector<int> weights;
+vector<int> weights2;
+vector<vector<int>> V;
 
+int knapSack();
+int knapSackAuxRec(int i, int i2, int j, int j2);
+void exit_gracefully();
 
-int knapSack(int W, int n, vector<int>& values, vector<int>& weights, vector<vector<int>>& V) {
+void exit_gracefully(){
+    ofstream detection_log("/tmp/knapsack/ga-detection.log");
+    detection_log << "Erro dectado!";
+    detection_log.close();
+    exit(0);
+}
+
+int knapSack() {
 
     V = vector<vector<int>>(n + 1);
 
     for (int i = 0; i <= n; i++)
         V[i] = vector<int>(W + 1);
 
-    for (int i = 0; i <= n; i++) {
-        for (int w = 0; w <= W; w++) {
+    int i = 0, i2 = 0;
+    while (i <= n && i == i2) {
+        int w = 0, w2 = 0;
+
+        while(w <= W && w == w2) {
             if (i == 0 || w == 0) {
                 V[i][w] = 0;
             } else {
                 V[i][w] = -1;
             }
+            w++; w2++;
         }
+
+        if(w != w2) {
+            exit_gracefully();
+        }
+
+        i++; i2++;
     }
 
-    V[n][W] = knapSackAuxRec(n, W, values, weights, V);
+    if(i != i2) {
+        exit_gracefully();
+    }
+
+    V[n][W] = knapSackAuxRec(n, n2, W, W2);
 
     return V[n][W];
 }
 
-int knapSackAuxRec(int i, int j, vector<int>& values, vector<int>& weights, vector<vector<int>>& V) { // i = n, j = w
+int knapSackAuxRec(int i, int i2, int j, int j2) { // i = n, j = w
     int value;
+
     if (V[i][j] < 0) { // meaning not already calculated
 
         if (j < weights[i]) {
-            value = knapSackAuxRec(i - 1, j, values, weights, V);
+            value = knapSackAuxRec(i - 1, i2 - 1, j, j2);
         } else {
-            value = max(knapSackAuxRec(i - 1, j, values, weights, V), values[i] + knapSackAuxRec(i - 1, j - weights[i], values, weights, V));
+            value = max(knapSackAuxRec(i - 1, i2 - 1, j, j2), values[i] + knapSackAuxRec(i - 1, i2 - 1, j - weights[i], j2 - weights[i]));
         }
 
-
         V[i][j] = value;// put valid value in the table for both cases
+    }
+
+    if(i != i2 || j != j2) {
+        exit_gracefully();
     }
 
     return V[i][j];
 }
 
-int executeAlgorithm() {
-    int W;
-    int n;
-    vector<int> values;
-    vector<int> weights;
-    vector<vector<int>> V;
-
+int main() {
     ifstream file("/tmp/knapsack/instance.txt");
+    ofstream outfile("/tmp/knapsack/outputmf");
 
     file >> n;
+    n2 = n;
     file >> W;
+    W2 = W;
 
     for (int i = 0; i < n; i++) {
         int x, y;
         file >> x >> y;
         values.push_back(x);
+        values2.push_back(x);
         weights.push_back(y);
+        weights2.push_back(y);
     }
 
-    return knapSack(W, n, values, weights, V);
-}
+	int result = knapSack();
 
-int main() {
-    ofstream outfile("/tmp/knapsack/outputmf");
+    if(n != n2 || W != W2) {
+        exit_gracefully();
+    }
 
-    int result;
-    int result1 = executeAlgorithm();
-    int result2 = executeAlgorithm();
-
-    if(result1 != result2) {
-	ofstream detection_log("/tmp/knapsack/mf-detection.log");
-        detection_log << result1 << " " << result2;
-    	outfile.close();
+    for(int i = 0; i < values.size(); i++){
+        if(values[i] != values2[i] || weights[i] != weights2[i]) {
+            exit_gracefully();
+        }
     }
 
     outfile << result;
     outfile.close();
-    cout << "Result: " << result << "\n";
-   
+
+	std::cout << "Result: " << result << "\n";
 
     return 0;
 }
